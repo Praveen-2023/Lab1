ce365f5506d046faca833c5185a42c36aa52336c
Praveen-2023 <143311712+Praveen-2023@users.noreply.github.com>
2025-02-06 05:43:08 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,57 +1,42 @@
+"""This script contains basic programs for numerical and geometric calculations."""
+
 # A program to check if a number is positive, negative, or zero
 
-
-number = 85986
-
-
-if number > 0:
+NUMBER = 85986
+if NUMBER > 0:
     print("The number is positive.")
-elif number < 0:
+elif NUMBER < 0:
     print("The number is negative.")
 else:
     print("The number is zero.")
-
-
 # A program to find the largest of two numbers
 
+NUM1 =6865
+NUM2 = 5676
 
-num1 =6865
-num2 = 5676
-
-
-if num1 > num2:
-    largest = num1
+if NUM1 > NUM2:
+    LARGEST = NUM1
 else:
-    largest = num2
+    LARGEST = NUM2
 
+print("The largest number is:", LARGEST)
 
-print("The largest number is:", largest)
+# A program to find Area of a rectangle
+LENGTH = 356
+WIDTH = 980
+AREA1 = LENGTH * WIDTH
+print("The area of the rectangle is:", AREA1)
 
+# A program to find Area of a rectangle
 
-# A program too fing Area of a rectangle
-
-
-length = 356
-width = 980
-area = length * width
-
-
-print("The area of the rectangle is:", area)
-
-
-# A program too fing Area of a rectangle
-
-
-radius = 698
-area = 3.14 * radius**2
-print("The area of the circle is:", area)
-
+RADIUS = 698
+AREA2 = 3.14 * RADIUS**2
+print("The area of the circle is:", AREA2)
 
 # A program to check if a word is a palindrome
 
-
-word = "kjbch"
-if word == word[::-1]:
-    print(f"{word} is a palindrome.")
+WORD = "kjbch"
+if WORD == WORD[::-1]:
+    print(f"{WORD} is a palindrome.")
 else:
-    print(f"{word} is not a palindrome.")
+    print(f"{WORD} is not a palindrome.")
